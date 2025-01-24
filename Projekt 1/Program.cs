b42a655de503db948f224e706ff8a9895754def7
Janan Rajagopal <Janan.Rajagopal@elev.ga.ntig.se>
2025-01-24 10:50:20 +0100

    wtgtws

--- Projekt 1/Program.cs
+++ Projekt 1/Program.cs
@@ -1,11 +1,39 @@
-﻿int hp  = 100; 
-int EnemyHp = 35; 
+﻿int enemyHp = 35; 
+player p1 = new();
+p1.hp = 100;
+p1.weapon = "sword";
+p1.minDamage = 3;
+p1.maxDamage = 7;
 
-while (hp > 1)
+
+Console.WriteLine("What is your Charecter name");
+string p1Name = Console.ReadLine();
+bool name = false;
+int checkName = 0;
+while (!name)
 {
-    hp--;
+    Console.WriteLine("Write your name (with letters!)");
+    name = int.TryParse(p1Name, out checkName);
+    Console.ReadLine();
 }
 
 
 
 Console.ReadLine();
+
+static void combat(player player )
+{
+    int attack = Random.Shared.Next(4);
+    if (attack > 2)
+    {
+        Console.WriteLine("");
+    }
+
+}
+class player 
+{
+    public int hp;
+    public string weapon;
+    public int minDamage;
+    public int maxDamage;
+}
